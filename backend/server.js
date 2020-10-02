aaf7f0f2cac0282f8d14fc0f25b0654c9e1780da
Xavier Hoskins <xavierdhoskins@gmail.com>
2020-10-02 11:30:02 -0500

    added models to the backend

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,27 @@
-const express = require('express');
-const products = require('./data/products');
+import express from 'express';
+import dotenv from 'dotenv';
+import colors from 'colors';
+import connectDB from './config/db.js';
+
+import productRoutes from './routes/productRoutes.js';
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('API is running....');
+  res.send('API is running...');
 });
 
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
+app.use('/api/products', productRoutes);
 
-app.listen(5000, console.log('Server running on port 5000'));
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,21 @@
-import React, { Fragment } from 'react';
-import products from '../products';
+import React, { Fragment, useState, useEffect } from 'react';
 import Product from '../components/Product';
+import axios from 'axios';
 import { Row, Col } from 'react-bootstrap';
 
 const HomeScreen = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const { data } = await axios.get('/api/products');
+
+      setProducts(data);
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <Fragment>
       <h1>Latest Products</h1>
