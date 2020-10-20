7d2d66deb6e343822ce71c24aa7377e1fc847159
Xavier Hoskins <xavierdhoskins@gmail.com>
2020-10-20 17:50:57 -0500

    added search bar functionality and starting on pagination

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,10 @@ import path from 'path';
 import express from 'express';
 import dotenv from 'dotenv';
 import colors from 'colors';
+import morgan from 'morgan';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
+
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
@@ -14,6 +16,10 @@ dotenv.config();
 connectDB();
 
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use(express.json());
 
